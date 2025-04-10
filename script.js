96c6a3e56d800b800e4e2aaf26a76d4551482dfa
regicidekk <148468076+regicidekk@users.noreply.github.com>
2025-04-10 23:57:53 +0500

    whitetheme++

--- script.js
+++ script.js
@@ -7,6 +7,22 @@ function themewhite() {
     document.getElementById('body').style.backgroundSize = "cover"
     document.getElementById('alogo').src = "whitelogo.png"
     document.getElementById('tt').style.color="black"
+    //projects
+    document.getElementById('tt').style.color="black"
+    document.getElementById('prjcts').style.backgroundColor="#c9c9c9"
+    document.getElementById('prjcts').style.color="#363636";
+    document.querySelectorAll('.headprj a').forEach(element => {
+      element.style.color = "#363636";
+    document.querySelectorAll('#imgmath').forEach(img => {
+      img.style.border = "1vh dashed #363636";
+    });
+    });
+    //about
+    document.getElementById('about').style.color="#212121"
+    document.getElementById('about').style.backgroundColor="aliceblue"
+    document.querySelectorAll('.abh').forEach(element => {
+        element.style.color = "#212121";
+    });
     themevar = true
   }
   else {
@@ -14,6 +30,21 @@ function themewhite() {
     document.getElementById('body').style.backgroundSize = "auto"
     document.getElementById('alogo').src = "148468076.png"
     document.getElementById('tt').style.color="white"
+    //projects
+    document.getElementById('prjcts').style.backgroundColor="#363636"
+    document.getElementById('prjcts').style.color="#c9c9c9"
+    document.querySelectorAll('.headprj a').forEach(element => {
+      element.style.color = "#c9c9c9";
+    document.querySelectorAll('#imgmath').forEach(img => {
+      img.style.border = "1vh dashed #c9c9c9";
+    });
+    });
+    //about
+    document.getElementById('about').style.color="aliceblue"
+    document.getElementById('about').style.backgroundColor="#212121"
+    document.querySelectorAll('.abh').forEach(element => {
+        element.style.color = "aliceblue";
+    });
     themevar = false
   }
 }
